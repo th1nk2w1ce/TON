6a5ce9ef948d076b782a9e4ae5371610e2506d46
Gusarich <danielsedovzzz@gmail.com>
2023-05-15 09:01:54 +0300

    secondary jetton wrapper && tests

--- tests/SecondaryJetton.spec.ts
+++ tests/SecondaryJetton.spec.ts
@@ -1,38 +1,86 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, Treasury, TreasuryContract } from '@ton-community/sandbox';
 import { Cell, toNano } from 'ton-core';
 import { SecondaryJetton } from '../wrappers/SecondaryJetton';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import { SecondaryJettonWallet } from '../wrappers/SecondaryJettonWallet';
 
 describe('SecondaryJetton', () => {
-    let code: Cell;
+    let minterCode: Cell;
+    let walletCode: Cell;
 
     beforeAll(async () => {
-        code = await compile('SecondaryJetton');
+        minterCode = await compile('SecondaryJetton');
+        walletCode = await compile('SecondaryJettonWallet');
     });
 
     let blockchain: Blockchain;
     let secondaryJetton: SandboxContract<SecondaryJetton>;
+    let wallets: SandboxContract<TreasuryContract>[];
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+        wallets = await blockchain.createWallets(10);
 
-        secondaryJetton = blockchain.openContract(SecondaryJetton.createFromConfig({}, code));
-
-        const deployer = await blockchain.treasury('deployer');
-
-        const deployResult = await secondaryJetton.sendDeploy(deployer.getSender(), toNano('0.05'));
-
+        secondaryJetton = blockchain.openContract(
+            SecondaryJetton.createFromConfig(
+                {
+                    admin: wallets[0].address,
+                    content: Cell.EMPTY,
+                    walletCode,
+                },
+                minterCode
+            )
+        );
+        const deployResult = await secondaryJetton.sendDeploy(wallets[0].getSender(), toNano('0.05'));
         expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
+            from: wallets[0].address,
             to: secondaryJetton.address,
             deploy: true,
             success: true,
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and secondaryJetton are ready to use
+    it('should deploy', async () => {});
+
+    it('should mint', async () => {
+        await secondaryJetton.sendMint(
+            wallets[0].getSender(),
+            toNano('0.05'),
+            toNano('0.01'),
+            wallets[1].address,
+            toNano('100')
+        );
+        const wallet = blockchain.openContract(
+            SecondaryJettonWallet.createFromAddress(await secondaryJetton.getWalletAddressOf(wallets[1].address))
+        );
+        expect(await wallet.getJettonBalance()).toEqual(toNano('100'));
+    });
+
+    it('should transfer', async () => {
+        await secondaryJetton.sendMint(
+            wallets[0].getSender(),
+            toNano('0.05'),
+            toNano('0.01'),
+            wallets[1].address,
+            toNano('100')
+        );
+        const wallet = blockchain.openContract(
+            SecondaryJettonWallet.createFromAddress(await secondaryJetton.getWalletAddressOf(wallets[1].address))
+        );
+
+        await wallet.sendTransfer(
+            wallets[1].getSender(),
+            toNano('0.05'),
+            toNano('0.01'),
+            wallets[2].address,
+            toNano('30')
+        );
+
+        expect(await wallet.getJettonBalance()).toEqual(toNano('70'));
+        const secondWallet = blockchain.openContract(
+            SecondaryJettonWallet.createFromAddress(await secondaryJetton.getWalletAddressOf(wallets[2].address))
+        );
+        expect(await secondWallet.getJettonBalance()).toEqual(toNano('30'));
     });
 });
--- wrappers/SecondaryJetton.ts
+++ wrappers/SecondaryJetton.ts
@@ -1,9 +1,18 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type SecondaryJettonConfig = {};
+export type SecondaryJettonConfig = {
+    admin: Address;
+    content: Cell;
+    walletCode: Cell;
+};
 
 export function secondaryJettonConfigToCell(config: SecondaryJettonConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeCoins(0)
+        .storeAddress(config.admin)
+        .storeRef(config.content)
+        .storeRef(config.walletCode)
+        .endCell();
 }
 
 export class SecondaryJetton implements Contract {
@@ -25,5 +34,44 @@ export class SecondaryJetton implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendMint(
+        provider: ContractProvider,
+        via: Sender,
+        value: bigint,
+        forwardValue: bigint,
+        recipient: Address,
+        amount: bigint
+    ) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(21, 32)
+                .storeUint(0, 64)
+                .storeAddress(recipient)
+                .storeCoins(forwardValue)
+                .storeRef(
+                    beginCell()
+                        .storeUint(0x178d4519, 32)
+                        .storeUint(0, 64)
+                        .storeCoins(amount)
+                        .storeAddress(this.address)
+                        .storeAddress(this.address)
+                        .storeCoins(0)
+                        .storeUint(0, 1)
+                        .endCell()
+                )
+                .endCell(),
+            value: value + forwardValue,
+        });
+    }
+
+    async getWalletAddressOf(provider: ContractProvider, address: Address) {
+        return (
+            await provider.get('get_wallet_address', [
+                { type: 'slice', cell: beginCell().storeAddress(address).endCell() },
+            ])
+        ).stack.readAddress();
     }
 }
